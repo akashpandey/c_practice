1a23a57364ece02ef5dcfe9c780d4d5ad5732700
akashpandey <akash_crash@rediffmail.com>
2013-08-22 23:22:14 +0530

    applied patch,removed bugs,now running correctly

--- 22.c
+++ 22.c
@@ -1,6 +1,6 @@
 //traversing in trees.
 #include<stdio.h>
-
+#include<malloc.h>
 struct tnode
 {
 int data;
@@ -15,12 +15,9 @@ struct tnode* insert(struct tnode*,int data);
 
 //------------------------------------------------------------------------------------------------------------
 
-
-
-
-
 struct Arrayqueue{
-int front,rear,capacity,*array;
+int front,rear,capacity;
+struct tnode** array;
 };
 
 struct Arrayqueue *Queue(int size)
@@ -28,7 +25,7 @@ struct Arrayqueue *Queue(int size)
 struct Arrayqueue *q = malloc(sizeof(struct Arrayqueue));
 q->front=q->rear=-1;
 q->capacity=size;
-q->array=malloc(q->capacity * sizeof(int));
+q->array= (struct tnode**)malloc(q->capacity*sizeof(struct tnode*));
 if(!q->array)
 return NULL;
 else
@@ -46,36 +43,36 @@ int Isfullqueue(struct Arrayqueue *q)
 return((q->rear+1)%q->capacity==q->front);
 }
 
-void enqueue(struct Arrayqueue *q,int data)
+void enqueue(struct Arrayqueue *q,struct tnode* data)
 {
 if(Isfullqueue(q))
 printf("\noverflow");
 else
 {
-q->rear=(q->rear+1)%q->capacity;
+q->rear=(q->rear+1)%(q->capacity);
 q->array[q->rear]=data;
 if(q->front==-1)
 q->front=q->rear;
 }
 }
 
-int dequeue(struct Arrayqueue *q)
+struct tnode* dequeue(struct Arrayqueue *q)
 {
-int data;
+struct tnode* data;
 if(Isemptyqueue(q))
 {
 printf("\nEMPTY");
 return 0;
 }
 else
-{
-data=q->array[q->front];
-if(q->front==q->rear)
-q->front=q->rear=-1;
-else
-q->front=(q->front+1)%q->capacity;
+	{
+	data=q->array[q->front];
+	if(q->front==q->rear)
+		q->front=q->rear=-1;	
+	else
+		q->front=(q->front+1)%q->capacity;
 return data;
-}
+	}
 }
 
 void deletequeue(struct Arrayqueue *q)
@@ -85,23 +82,13 @@ if(q)
 if(q->array)
 free(q->array);
 free(q);
-printf("Ab bas kar bhai!!\n");
 }
 }
-
-
-
-
-
-
-
-
-
-
 
 //---------------------------------------------------------------------------------------------------------------------------
 main()
 {
+
 int choice=0,data;
 printf("Keep entering the digits: press -1 to stop!!\n");
 printf("Start entering!!\n");
@@ -136,13 +123,14 @@ switch(choice)
 	break;
 	}
 }while(choice<4);
+
 }
 //----------------------------------------------------------------------------------------------------------------------
 //Insert Function.
 struct tnode* insert(struct tnode* a,int data)
 {
 	
-	struct Arrayqueue* Q;	
+		
 	struct tnode* temp;
 	struct tnode* newnode;
 	newnode=(struct tnode*) malloc(sizeof(struct tnode));
@@ -158,26 +146,24 @@ struct tnode* insert(struct tnode* a,int data)
 			a=newnode;
 			return a;
 			}
-	Q = Queue(20);
-	enqueue(Q,a->data);
+	struct Arrayqueue *Q=Queue(20);
+	enqueue(Q,a);
 	while(!Isemptyqueue(Q))
 	{
 		temp = dequeue(Q);
 		if(temp->left)
-			enqueue(Q,temp->left->data);
+			enqueue(Q,temp->left);
 		else
 		{
 			temp->left=newnode;
-			deletequeue(Q);
 			break;		
 		}
 		
 		if(temp->right)
-			enqueue(Q,temp->right->data);
+			enqueue(Q,temp->right);
 		else
 		{
 			temp->right=newnode;
-			deletequeue(Q);
 			break ;		
 		}
 	}
